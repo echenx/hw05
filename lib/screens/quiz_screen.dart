f5cc2ec291af88331eba0344f791f7e046ff5529
evan <evanchenx7@gmail.com>
2024-11-30 01:42:15 -0500

    added timer

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import '../models/question.dart';
 import '../services/api_service.dart';
@@ -27,11 +28,19 @@ class _QuizScreenState extends State<QuizScreen> {
   bool _answered = false;
   String _selectedAnswer = "";
   String _feedbackText = "";
+  late Timer _timer;
+  int _timeLeft = 15;
 
   @override
   void initState() {
     super.initState();
     _loadQuestions();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
   }
 
   Future<void> _loadQuestions() async {
@@ -46,13 +55,36 @@ class _QuizScreenState extends State<QuizScreen> {
         _questions = questions;
         _loading = false;
       });
+      _startTimer();
     } catch (e) {
       print(e);
       // Handle error appropriately
     }
   }
 
+  void _startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (_timeLeft > 0) {
+        setState(() {
+          _timeLeft--;
+        });
+      } else {
+        _timer.cancel();
+        _markTimeout();
+      }
+    });
+  }
+
+  void _markTimeout() {
+    setState(() {
+      _answered = true;
+      _feedbackText =
+          "Time's up! The correct answer is ${_questions[_currentQuestionIndex].correctAnswer}.";
+    });
+  }
+
   void _submitAnswer(String selectedAnswer) {
+    _timer.cancel();
     setState(() {
       _answered = true;
       _selectedAnswer = selectedAnswer;
@@ -68,12 +100,18 @@ class _QuizScreenState extends State<QuizScreen> {
   }
 
   void _nextQuestion() {
-    setState(() {
-      _answered = false;
-      _selectedAnswer = "";
-      _feedbackText = "";
-      _currentQuestionIndex++;
-    });
+    if (_currentQuestionIndex < _questions.length - 1) {
+      setState(() {
+        _answered = false;
+        _selectedAnswer = "";
+        _feedbackText = "";
+        _currentQuestionIndex++;
+        _timeLeft = 15;
+      });
+      _startTimer();
+    } else {
+      _timer.cancel();
+    }
   }
 
   Widget _buildOptionButton(String option) {
@@ -129,6 +167,14 @@ class _QuizScreenState extends State<QuizScreen> {
                   ),
                 ),
               ],
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Time Left: $_timeLeft seconds',
+              style: TextStyle(
+                fontSize: 16,
+                color: _timeLeft <= 5 ? Colors.red : Colors.black,
+              ),
             ),
             SizedBox(height: 16),
             Text(
