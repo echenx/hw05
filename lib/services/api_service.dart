e7ec7d9eb67e1d3a071dec3892a1dd2c7c51220e
evan <evanchenx7@gmail.com>
2024-12-02 20:41:17 -0500

    fixed html encoded characters

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,6 +3,19 @@ import 'package:http/http.dart' as http;
 import '../models/question.dart';
 
 class ApiService {
+  static String _decodeHtml(String input) {
+    return input
+        .replaceAll('&quot;', '"')
+        .replaceAll('&amp;', '&')
+        .replaceAll('&lt;', '<')
+        .replaceAll('&gt;', '>')
+        .replaceAll('&#039;', "'")
+        .replaceAll('&rdquo;', '"')
+        .replaceAll('&ldquo;', '"')
+        .replaceAll('&shy;', '')
+        .replaceAll('&hellip;', '...');
+  }
+
   static Future<List<Question>> fetchQuestions({
     required int numQuestions,
     required String category,
@@ -17,9 +30,15 @@ class ApiService {
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      List<Question> questions = (data['results'] as List)
-          .map((questionData) => Question.fromJson(questionData))
-          .toList();
+      List<Question> questions = (data['results'] as List).map((questionData) {
+        questionData['question'] = _decodeHtml(questionData['question']);
+        questionData['correct_answer'] = _decodeHtml(questionData['correct_answer']);
+        questionData['incorrect_answers'] = (questionData['incorrect_answers'] as List)
+            .map((answer) => _decodeHtml(answer.toString()))
+            .toList();
+        
+        return Question.fromJson(questionData);
+      }).toList();
       return questions;
     } else {
       throw Exception('Failed to load questions');
